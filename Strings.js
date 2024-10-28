3b3ef76f0aebf6ca9b84598c5998924783e5ba61
Awais Ali <awaisali.skylinxtech@gmail.com>
2024-10-28 11:48:14 +0200

    Strings

---
+++ Strings.js
@@ -0,0 +1,29 @@
+let string = 'Hello! Hi!'
+
+console.log(string.includes('H'));
+
+// Get the individual char
+console.log(string.charAt(1));
+console.log(string[0]);
+
+//join strings
+console.log(string.concat(" Hi!"));
+
+//indexOf
+console.log(string.indexOf('!'));
+
+//lastIndexOf
+console.log(string.lastIndexOf('!'));
+
+//slice
+console.log(string.slice(1))
+console.log(string.slice(2,5))
+
+//toArray
+console.log(string.split('').map(x => x.toUpperCase()).join(''))
+
+//Apply colors to strings
+//install chalk; npm install chalk 
+//chalk.red('Hello') + chalk.blue('Hi'); print Hello in red color and Hi in blue
+//for background
+//chalk.bgGreen('Working'); print working with green bg color
